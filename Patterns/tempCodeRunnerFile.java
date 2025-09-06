0ec793667ad42579a013b885b9896d650820c4a2
Abhi37231 <159634392+Abhi37231@users.noreply.github.com>
2025-09-06 12:34:39 +0530

    06/09

--- Patterns/tempCodeRunnerFile.java
+++ Patterns/tempCodeRunnerFile.java
@@ -1,5 +1,3 @@
-   for(int i= 0;i<n;i++){
-            for(int j=1;j<=n-i;j++){
-                System.out.print("*");
+ for(int j = 1;j<=n-1;j++){
+                System.out.print(" ");
             }
-            System.out.println();
